ce1383566f15ee9c77671ee75bfdbd0d42534281
Sarthak Kukreja <kukreja_sarthak@yahoo.com>
2020-12-12 14:47:55 +0530

    delete-unsed-and-add-feed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import {
+  Button , Row, Col, Card, CardBody, CardTitle, CardLink , CardText, CardImg
+} from 'reactstrap';
+import './index.css'
+
+
+class ContentFeed extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      'items': []
+    }
+  }
+  componentDidMount() {
+    this.getItems();
+  }
+
+  getItems() {
+    fetch('http://127.0.0.1:8000/search/')
+    .then(results => results.json())
+    .then(results => this.setState({'items': results}))
+  }
+  render() {
+    return (
+      <ul>
+        {this.state.items.map(function(item, index) {
+          return <ContentItem item={item} key={index} />
+        })}
+      </ul>
+    );
+  }
+}
+
+const ContentItem = ({ item }) => (
+  <Row className="ContentItem">
+    <Col xs="3" />
+    <Col xs="12" sm="6">
+      <Card border="dark">
+        <CardImg top width="100%" src={item.thumbnail_url}></CardImg>
+        <CardBody>
+          <CardTitle>
+            <h3> {item.title} </h3>
+          </CardTitle>
+          <CardText>
+            {item.description}
+          </CardText>
+          <CardLink href={"https://www.youtube.com/watch?v=" + item.video_id }>Go to Video</CardLink>
+        </CardBody>
+      </Card>
+    </Col>
+  </Row>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ContentFeed />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
